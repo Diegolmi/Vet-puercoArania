f362e7e0d602767d1bff8a3c5742c22112b81289
Gaby Flores <gabrielaflores671@gmail.com>
2020-05-18 22:54:18 -0300

    modificacion de footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
