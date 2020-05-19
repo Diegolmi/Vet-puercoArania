838eb19e0cb84d55443994f9cd1958583306bfe6
Diego Olmi <57357345+Diegolmi@users.noreply.github.com>
2020-05-19 02:53:48 -0300

    Rama gaby (#2)
    * modificacion del footer
    
    * modificacion de footer
    
    Co-authored-by: Gaby Flores <gabrielaflores671@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
+
+import Footer from './components/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './components/About/about';
 
 
 
+
 function App() {
   return (
     <div className="App">
+
+      <header className="App-header">
+    
+      </header>
+      <Footer/>
+
       <>
           <About/>
       </>
+
     </div>
+    
+    
   );
 }
  
