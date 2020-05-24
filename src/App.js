fd895f290bdc91cc25988b87347140fa8bf5cfc4
Diego <diegolmi@hotmail.com>
2020-05-24 04:03:27 -0300

    mediaQueries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../src/assets/img/logo.png'
-import Registro from './components/registro'
-import Nav from '../src/components/Nav'
-
-
+import 'mdbreact/dist/css/mdb.css';
+// import Registro from './components/registro'
+import Navbar from './components/Navbar/Nav'
 import Footer from './components/Footer/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './components/About/about';
 import EcommerceHome from './components/EcommerceHome/EcommerceHome';
-// import logo from '../src/assets/img/logo.png'
-// import Registro from './components/registro'
-import './App.css';
-import 'mdbreact/dist/css/mdb.css';
 import Servicios from './components/Servicios/Servicios';
 
 
@@ -29,15 +19,14 @@ function App() {
 
       <header className="App-header">
 
-        <Nav />
-        <img
-              className="img-fluid"
-              src={logo} alt=""
-            />
+        <Navbar />
+      
         
       </header>
       <section>
-     <Registro />
+        <EcommerceHome />
+      <Servicios />
+     {/* <Registro /> */}
       </section>
 
     
