0e814f99a2fa150751d5ee130321cfde58c2adab
CrisT <crisostomotorres@hotmail.com>
2020-05-25 18:21:51 -0300

    Created Breadcrumbs folder along initial index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SideNavPage from './components/SidebarE-commerce';
 import './App.css';
+import SimpleBreadcrumbs from './components/Breadcrumbs';
 
 function App() {
   return (
-    <SideNavPage />
+    // <SideNavPage />
+    <SimpleBreadcrumbs />
     );
 }
 
--- src/components/SidebarE-commerce/index.js
+++ src/components/SidebarE-commerce/index.js
@@ -1,36 +1,40 @@
 import React from 'react';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
+import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
 
 //styles
 import './style.css'
 
 export default function RadioButtonsGroup() {
-  const [value, setValue] = React.useState('');
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
 
   return (
     <div className="sidenav-filters">
       <FormControl component="fieldset">
-        <FormLabel component="legend">Tipo de animal: </FormLabel>
-        <RadioGroup aria-label="type" name="type" value={value} onChange={handleChange}>
-          <FormControlLabel value="dogs" control={<Radio />} label="Perros" />
-          <FormControlLabel value="cats" control={<Radio />} label="Gatos" />
+        <FormLabel component="legend">Tipo de animal:</FormLabel>
+        <RadioGroup aria-label="type" name="animal-type">
+          <FormControlLabel value="dog" control={<Radio />} label="Dog" />
+          <FormControlLabel value="cats" control={<Radio />} label="Cats" />
         </RadioGroup>
-      </FormControl>
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Tamaño: </FormLabel>
-        <RadioGroup aria-label="size" name="size" value={value} onChange={handleChange}>
-          <FormControlLabel value="small" control={<Radio />} label="Chico" />
-          <FormControlLabel value="medium" control={<Radio />} label="Mediano" />
-          <FormControlLabel value="big" control={<Radio />} label="Grande" />
-        </RadioGroup>
+        <FormLabel component="legend">Tamaño:</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Chico"
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Mediano"
+          />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Grande"
+          />
+        </FormGroup>
       </FormControl>
     </div>
   );
