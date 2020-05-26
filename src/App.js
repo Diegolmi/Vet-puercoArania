063bc463d9eef7789e933726dfb1f1d3d022e3ac
Diego Olmi <57357345+Diegolmi@users.noreply.github.com>
2020-05-25 23:23:49 -0300

    Diego navbar (#12)
    * Navbar
    
    * logo
    
    * Turno
    
    * mediaQueries
    
    * carousel
    
    * Navbar Master
    
    * Css de la master
    
    * register
    
    * Menu Hamburguesa
    
    * Register

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
+
 import SideNavPage from './components/SidebarE-commerce';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import CarouselPage from './components/Carousel/index';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Nav from './components/Navbar/Nav'
+// import Registro from './components/registro'
+import Navbar from './components/Navbar/Nav'
 import Footer from './components/Footer/Footer';
+
+import './App.css';
+import About from './components/About/about';
+import EcommerceHome from './components/EcommerceHome/EcommerceHome';
+import Servicios from './components/Servicios/Servicios';
+import Carousel from './components/Carousel';
+import Register from './components/Register/Register';
+
+
 // import About from './components/About/about';
 import EcommerceHome from './components/EcommerceHome/EcommerceHome';
 // import logo from '../src/assets/img/logo.png'
@@ -22,9 +29,42 @@ import SimpleBreadcrumbs from './components/Breadcrumbs';
 
 function App() {
   return (
+
+    <div className="container-fluid">
+
+      <header className="App-header">
+
+        <Navbar />
+      
+        
+      </header>
+      <section>
+        <Carousel />
+        <EcommerceHome />
+      <Servicios />
+     {/* <Registro /> */}
+      </section>
+
+    
+      
+      <Footer/>
+
+      <>
+          <About/>
+          <Register />
+
+      </>
+
+
+    </div>
+    
+    
+  );
+
     // <SideNavPage />
     <SimpleBreadcrumbs />
     );
+
 }
 
 export default App;
--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, } from
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView} from
   "mdbreact";
 
 //styles
