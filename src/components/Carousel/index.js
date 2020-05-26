b4b5ee549c0f7911ea739ca4384e96da40ccd2cc
Diego Olmi <57357345+Diegolmi@users.noreply.github.com>
2020-05-25 23:57:13 -0300

    Fixed carousel, changed images to better fit the landing page (#14)
    Co-authored-by: CrisT <crisostomotorres@hotmail.com>

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
+"mdbreact";
 import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView } from
   "mdbreact";
 
@@ -9,8 +11,7 @@ import './style.css'
 import {
   img1,
   img2,
-  banner,
-  banner2
+  img3
 } from "../../assets/img/index";
 
 
@@ -18,6 +19,62 @@ const CarouselPage = () => {
   return (
     <div className="container-fluid contenedorCarousel">
       <MDBCarousel
+
+      activeItem={1}
+      length={3}
+      showControls={true}
+      showIndicators={true}
+      className="z-depth-1"
+    >
+      <MDBCarouselInner>
+        <MDBCarouselItem itemId="1">
+          <MDBView>
+            <img
+              className="d-block w-100"
+              src={img1}
+              alt="Second slide"
+            />
+          <MDBMask overlay="black-light" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h3 className="h3-responsive">Servicios para gatos y perros</h3>
+            <p>¡Y también productos alimenticios, accesorios, juguetes y todo lo que tu mascota necesita!</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId="2">
+          <MDBView>
+            <img
+              className="d-block w-100"
+              src={img2}
+              alt="Third slide"
+            />
+          <MDBMask overlay="black-light" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h3 className="h3-responsive">Estetica</h3>
+            <p>¡Baño, peluquería, cuidado higienico y mucho más!</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId="3">
+          <MDBView>
+            <img
+              className="d-block w-100"
+              src={img3}
+              alt="Second slide"
+            />
+          <MDBMask overlay="black-light" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h3 className="h3-responsive">Atención médica y consultas.</h3>
+            <p>¡Urgencias, internaciones, turnos para consulta y mucho más!</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+      </MDBCarouselInner>
+    </MDBCarousel>
+    </MDBContainer>
+  );
+}
+
         activeItem={4}
         length={4}
         showControls={true}
@@ -66,5 +123,6 @@ const CarouselPage = () => {
     </div>
   );
 }
+
 
 export default CarouselPage;
