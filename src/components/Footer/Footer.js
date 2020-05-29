d4f437cca3ab81f86224e90872a12805dd171467
Gaby Flores <gabrielaflores671@gmail.com>
2020-05-29 00:29:41 -0300

    footer responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter,} from "mdbreact";
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import 'mdbreact/dist/css/mdb.css';
 import puercoaraña from './puercoaraña.png';
 import'./footer.css';
 
 
+
 const Footer = () => {
   return (
     <div className="footer">
-    <MDBFooter id="fondo" color="mdb-color" className=" font-small mt-2 pt-4 ">
-      <MDBContainer fluid className=" text-center text-md-left">
-        <MDBRow >
-          <MDBCol md="3 "  className="imagenhomero"> 
+    <MDBFooter  color="mdb-color" className="font-small mt-2 pt-4 ">
+      <MDBContainer fluid  className=" text-center text-md-left">
+        <MDBRow  >
+          <MDBCol md="4 "  className="imagenhomero"> 
             <img src={puercoaraña} className="img" alt="logo" />
           </MDBCol>
           <hr className="clearfix w-100 d-md-none"/>
-          <MDBCol md="2" lg="3"   >
+          <MDBCol  lg="2"   >
             <div className="informacion">
             <h5 className="text-uppercase mb-4 font-weight-bold">Información</h5>
-            <div className=" d-flex flex-column ">
+            <div className=" d-flex flex-column  ">
             <div  className="d-flex ">
             <img src="https://pngimage.net/wp-content/uploads/2018/06/icono-de-direccion-png.png" className="iconos" alt="logo" />
             <p className="m-2 p-1" >
@@ -29,7 +29,7 @@ const Footer = () => {
             <div id="gmail" className="d-flex ">
             <img src="https://i.pinimg.com/originals/84/7c/08/847c083cc09040091439e3c05d1fedde.png" className="iconos" alt="logo" />
             <p className="m-2 p-1">
-               puercoarañaoficial@gmail.com
+               puercoaraña@gmail.com
             </p>
             </div>
             <div id="celular" className="d-flex">
@@ -62,6 +62,7 @@ const Footer = () => {
               </a>
               </div>
              </div>
+             <div className="secciones">
              <div className="links d-flex flex-column p-4">
                <h5 className="text-uppercase mb-4 font-weight-bold  ">Secciones</h5>
                <div className="columnas d-flex justify-content-center">
@@ -75,9 +76,6 @@ const Footer = () => {
               </li>
               <li className="list-unstyled d-flex justify-content-start p-1">
                 <a href="#!">cuenta</a>
-              </li>
-              <li className="list-unstyled d-flex justify-content-start p-1">
-                <a href="#!">seguimiento</a>
               </li>
             </ul>
             </div>
@@ -99,11 +97,12 @@ const Footer = () => {
             </div>
             </div>
             </div>
+            </div>
              </div>
-             <div className="ubicacion">
-              <div  md="5" >
+             
+             <div className="ubicacion d-flex flex-column">
               <h5 id="texto" className="text-uppercase mb-4 font-weight-bold">   ubicación   </h5>
-             <div id="map-container"className=" rounded z-depth-1-half map-container " >
+             <div id="map-container"className=" rounded z-depth-1-half map-container" >
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1049265915804!2d-65.20936748545087!3d-26.83661479649731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses!2sar!4v1589831719322!5m2!1ses!2sar"
               title="This is a unique title"
@@ -112,7 +111,7 @@ const Footer = () => {
               frameBorder="0"
             />
           </div>
-              </div>
+              
               </div>  
               </div>
           </MDBCol>
