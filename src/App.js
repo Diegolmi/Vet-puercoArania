929b0ffa6c328db1a851ed61ff31c0d05d9d595a
CrisT <crisostomotorres@hotmail.com>
2020-05-30 14:09:18 -0300

    Form completed

--- src/App.js
+++ src/App.js
@@ -3,60 +3,18 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import CarouselPage from './components/Carousel/index';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Nav from './components/Navbar/Nav'
-import Footer from './components/Footer/Footer';
-// import About from './components/About/about';
-import EcommerceHome from './components/EcommerceHome/EcommerceHome';
-// import logo from '../src/assets/img/logo.png'
-// import Registro from './components/registro'
 import './App.css';
 import 'mdbreact/dist/css/mdb.css';
-import Servicios from './components/Servicios/Servicios';
+import FormPage from './components/FormServicios';
 
 
 
 
 function App() {
   return (
-
-    <Container fluid>
-      <Row className="rowNav">
-        <Col>
-          <Nav />
-        </Col>
-      </Row>
-      <Row className="rowCarousel">
-        <Col>
-          <CarouselPage />
-        </Col>
-      </Row>
-      <div className="rowEcommerce">
-        
-          <EcommerceHome />
-        
-        </div>
-        <Row className="rowServicios">
-        <Col>
-          <Servicios />
-        </Col>
-      
-      </Row>
-      <Row className="rowFooter">
-        <Col>
-          <Footer />
-        </Col>
-      </Row>
-    </Container>
-
-
-
-
-
-
+    <div>
+      <FormPage />
+    </div>
   );
 }
 
