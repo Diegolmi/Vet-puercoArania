ce86ac5f196c24af991a8667d92ef9f30e89e0a6
CrisT <crisostomotorres@hotmail.com>
2020-07-03 00:21:16 -0300

    Agregado el form y la funcion de axios put

--- src/components/Usser/Components/ModificarDatos.js
+++ src/components/Usser/Components/ModificarDatos.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBInput, MDBBtn } from "mdbreact";
-
+import axiosInstance from '../../util/axiosInstance';
 
 const ModificarDatos = () => {
-    return ( 
-        <form>
-            <MDBInput label="Nombre" />
-            <MDBInput label="Apellido" />
-            <MDBInput label="Email" />
-            <MDBInput label="Domicilio" />
-            <MDBInput label="Contraseña" type="password" />
-            <MDBInput label="Confirmar Contraseña" type="password"/>
-            <MDBBtn color="indigo">Modificar</MDBBtn>
+    const [editarDatos, setEditarDatos] = useState({
+        username: '',
+        name: '',
+        lastname: '',
+        email: '',
+        password: '',
+        repassword: '',
+        address: ''
+    })
+
+    const handleChange = e => {
+        setEditarDatos({
+            ...editarDatos,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = id => async e => {
+        e.preventDefault();
+        const result = await axiosInstance.put(`/private/user/${id}`, editarDatos);
+        console.log(result)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <MDBInput onChange={handleChange} name="username" label="Nombre de usuario" required error="wrong" success="right" validate group />
+            <MDBInput onChange={handleChange} name="name" label="Nombre" type="text" required error="wrong" success="right" validate group />
+            <MDBInput onChange={handleChange} name="lastname" label="Apellido" type="text" required error="wrong" success="right" validate group />
+            <MDBInput onChange={handleChange} name="email" label="Email" type="email" required error="wrong" success="right" validate group />
+            <MDBInput onChange={handleChange} name="address" label="Domicilio" required error="wrong" success="right" validate group />
+            <MDBInput onChange={handleChange} name="password" label="Contraseña" type="password" required validate group />
+            <MDBInput onChange={handleChange} name="repassword" label="Confirmar Contraseña" type="password" required validate group />
+            <MDBBtn type="button" color="indigo">Modificar</MDBBtn>
         </form>
-     );
+    );
 }
- 
-export default ModificarDatos
+
+export default ModificarDatos;
