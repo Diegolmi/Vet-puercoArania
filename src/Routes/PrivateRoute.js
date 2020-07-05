c4c86e39ddfe52cdab95eda4704171ec5d6451fd
martin jerez <martincho111986@gmail.com>
2020-07-05 03:18:42 -0300

    recibiendo ultimos push del grupo

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Route, Redirect} from 'react-router-dom';
-import isAuthenticate from '../components/util/isAuthenticate';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import isAuthenticate from "../components/util/isAuthenticate";
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const decoded = isAuthenticate();
+  if (!decoded) 
+    return <Route {...rest} render={(props) => <Redirect to="/login" />} />;
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route 
-        {...rest}
-        render={(props)=> isAuthenticate() ? <Component {...props}/> : <Redirect to="/login" />}
+  return (
+    <Route
+      {...rest}
+      render={(props) => <Component {...props} user={decoded} />}
     />
-)
- 
+  );
+};
+
 export default PrivateRoute;
